d9fa062bea93fe42f0b24f705c959201c7c0a414
Guilherme Sousa <143344186+ArrozDoce007@users.noreply.github.com>
2024-10-13 22:14:47 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -9,7 +9,7 @@ import hashlib
 
 app = Flask(__name__, static_folder='static')  # Configura o diretório estático
 CORS(app)  # Habilita CORS para todas as rotas
-app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), '..', 'static', 'uploads') # Defina o diretório de upload
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')  # Defina o diretório de upload
 
 # Verifica se o diretório de uploads existe, se não existir, cria
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
@@ -22,9 +22,9 @@ processed_requests = set()
 def get_db_connection():
     try:
         db = mysql.connector.connect(
-            host="database-1.c9ec8o0ioxuo.us-east-2.rds.amazonaws.com",
-            user="admin",
-            password="26042004",
+            host="localhost",
+            user="root",
+            password="2004",
             database="lab_reservation"
         )
         return db
