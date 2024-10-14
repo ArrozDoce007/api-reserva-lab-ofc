628be2aac67e0fd2a4f227d610c5350c956f8582
Guilherme Sousa <143344186+ArrozDoce007@users.noreply.github.com>
2024-10-14 20:22:09 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -12,6 +12,14 @@ import threading
 app = Flask(__name__)
 CORS(app)
 
+# Limitar o tamanho do upload para 10 MB
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10 MB
+
+# Gerenciamento de erros para arquivo muito grande
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    return jsonify({'error': 'O arquivo enviado é muito grande. O tamanho máximo permitido é de 10 MB.'}), 413
+
 sala_lock = threading.Lock()
 
 # Configurações do S3
