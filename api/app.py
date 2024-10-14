3b1c0b538a762eaaed6bc0f8e851e4ebc7fe61f7
Guilherme Sousa <143344186+ArrozDoce007@users.noreply.github.com>
2024-10-14 17:23:51 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from datetime import datetime
 from werkzeug.utils import secure_filename
-from botocore.exceptions import NoCredentialsError, ClientErro
 import mysql.connector
 import os
 import pytz
