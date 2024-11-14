4b11510c9da9359849ddc67e467f8f25eeb43ef3
Guilherme Sousa <143344186+ArrozDoce007@users.noreply.github.com>
2024-11-14 19:00:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,6 @@ CORS(app)
 # Limitar o tamanho do upload para 10 MB
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10 MB
 
-# Gerenciamento de erros para arquivo muito grande
-@app.errorhandler(413)
-def request_entity_too_large(error):
-    return jsonify({'error': 'O arquivo enviado é muito grande. O tamanho máximo permitido é de 10 MB.'}), 413
-
 # Registrar todos os blueprints
 for blueprint in all_blueprints:
     app.register_blueprint(blueprint)
