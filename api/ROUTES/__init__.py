5da200f97df7ea8232b5915aa71e45718545fdc8
Guilherme Sousa <143344186+ArrozDoce007@users.noreply.github.com>
2024-11-14 16:36:50 -0300

    Delete api/ROUTES directory

--- api/ROUTES/__init__.py
+++
@@ -1,9 +0,0 @@
-# routes/__init__.py
-from .reservation import reservation_bp
-from .room import room_bp
-from .login_cadastro import login_cadastro_bp
-from .user import user_bp
-from .hr import hr_bp
-
-# Lista de todos os blueprints para registro
-all_blueprints = [reservation_bp, room_bp, login_cadastro_bp, hr_bp, user_bp]
