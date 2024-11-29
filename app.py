c97ba2f3732832a0ec9fd6d52b4c1354dc9212bc
Gui <guilhermesousaesilva226@gmail.com>
2024-11-29 17:54:42 -0300

    teste

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-# app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ROUTES import all_blueprints
 
 app = Flask(__name__)
+
+# Adicionar uma verificação no cabeçalho User-Agent
 @app.before_request
-def check_origin():
+def check_user_agent():
     if request.method == "OPTIONS":
         return  # Ignora as requisições OPTIONS (necessárias para CORS)
 
-    origin = request.headers.get('Origin')
-    if origin and origin != "https://reserva-salas-uninassau.netlify.app":
-        return jsonify({"message": "Acesso não permitido"}), 403  # Retorna erro 403 se a origem não for permitida
+    user_agent = request.headers.get('User-Agent', '')
+    if "Postman" in user_agent:
+        return jsonify({"message": "Acesso não permitido"}), 403  # Bloqueia requisições do Postman
 
 # Configurar CORS
 CORS(app, resources={r"/*": {"origins": "https://reserva-salas-uninassau.netlify.app"}})
-
 
 # Limitar o tamanho do upload para 10 MB
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10 MB
