afa44cbe1bb851f4f33d1b08428f3fb17cfac641
Gui <guilhermesousaesilva226@gmail.com>
2024-11-29 17:45:26 -0300

    gonfigurações de origens

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 # app.py
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ROUTES import all_blueprints
 
 app = Flask(__name__)
+@app.before_request
+def check_origin():
+    if request.method == "OPTIONS":
+        return  # Ignora as requisições OPTIONS (necessárias para CORS)
+
+    origin = request.headers.get('Origin')
+    if origin and origin != "https://reserva-salas-uninassau.netlify.app":
+        return jsonify({"message": "Acesso não permitido"}), 403  # Retorna erro 403 se a origem não for permitida
+
+# Configurar CORS
 CORS(app, resources={r"/*": {"origins": "https://reserva-salas-uninassau.netlify.app"}})
+
 
 # Limitar o tamanho do upload para 10 MB
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10 MB
